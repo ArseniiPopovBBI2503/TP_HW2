2b2b222e231559577df58657689c1f02788c17f1
Попов Арсений <arigpopov@edu.hse.ru>
2026-06-03 23:32:02 +0300

    Исправление ошибки в классе ShoppingList в методе get_list

--- src/ShoppingList.py
+++ src/ShoppingList.py
@@ -1,17 +1,17 @@
-import Ingredient
-import Recipe
+from src.Ingredient import Ingredient
+from src.Recipe import Recipe
 
 class ShoppingList:
     def __init__(self,):
         self._items = list()
     
     def add_recipe(self, recipe: Recipe, portions: float) -> None:
-        if float(portions) <= 0:
+        if portions <= 0:
             raise ValueError("Количество порций должно быть положительным")
         new_recipe = recipe.scale(portions)
-        for ingredient in new_recipe.ingredient:
-            self._itemps.append((ingredient,recipe.title))
-            
+        for ingredient in new_recipe.ingredients:
+            self._items.append((ingredient, recipe.title))
+
     def remove_recipe(self, title:str):
         new_items = list()
         for item in self._items:
@@ -24,7 +24,7 @@ class ShoppingList:
         for ingredient, title in self._items:
             key = (ingredient.name, ingredient.unit)
             if key in shop_list:
-                shop_list[key] += ingredient.quantity
+                shop_list[key].quantity += ingredient.quantity
             else:
                 shop_list[key] = Ingredient(ingredient.name, ingredient.quantity, ingredient.unit)
         sorted_items = sorted(shop_list.values(), key=lambda x: x.name)
